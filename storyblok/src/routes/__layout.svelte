a7eccc1af72b8fb91ec8e61de92c8b0b70d85a14
loghomefinishing <74892898+loghomefinishing@users.noreply.github.com>
2022-10-19 17:29:14 -0600

    first commit

---
+++ storyblok/src/routes/__layout.svelte
@@ -0,0 +1,29 @@
+<script context="module">
+  import { storyblokInit, apiPlugin } from "@storyblok/svelte";
+  import Feature from "../components/Feature.svelte";
+  import Grid from "../components/Grid.svelte";
+  import Page from "../components/Page.svelte";
+  import Teaser from "../components/Teaser.svelte";
+
+  storyblokInit({
+    accessToken: "RSkNyekAXsQyHoQzLj4o6Qtt",
+    apiOptions: {
+    region: "us", // Pass this key/value if your space was created under US region
+  },
+    use: [apiPlugin],
+    components: {
+      feature: Feature,
+      grid: Grid,
+      page: Page,
+      teaser: Teaser,
+    },
+  });
+</script>
+
+<script>
+  import "../app.css";
+</script>
+
+<main>
+  <slot />
+</main>
