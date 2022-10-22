8e5a80ed940d78f7f23acbf26e5793a13ba833c6
loghomefinishing <74892898+loghomefinishing@users.noreply.github.com>
2022-10-22 01:13:03 -0600

    tailwind

--- storyblok/src/components/Feature.svelte
+++ storyblok/src/components/Feature.svelte
@@ -8,6 +8,8 @@
 	<h1 class="text-2xl text-[#1d243d] font-bold">{blok.image}</h1>
 	<h1 class="text-2xl text-[#1d243d] font-bold">{blok.name}</h1>
 <p>vs and netlify hibye</p>
+<p class="text-3xl font-bold underline">
+    Hello world! </p>
 </div>
 <div><img src="{blok.pic.filename}" alt="dog"/></div>
 
