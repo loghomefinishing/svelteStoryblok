c8e777a87db8350ebeba6995f1ef922399ad98ba
loghomefinishing <74892898+loghomefinishing@users.noreply.github.com>
2022-10-21 22:25:03 -0600

    build

--- storyblok/svelte.config.js
+++ storyblok/svelte.config.js
@@ -1,5 +1,7 @@
 import preprocess from "svelte-preprocess";
 import adapter from "@sveltejs/adapter-auto";
+import vercel from '@sveltejs/adapter-vercel';
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -14,4 +16,23 @@ const config = {
   ],
 };
 
-export default config;
+export default {
+  kit: {
+    // default options are shown
+    adapter: vercel({
+      // if true, will deploy the app using edge functions
+      // (https://vercel.com/docs/concepts/functions/edge-functions)
+      // rather than serverless functions
+      edge: false,
+
+      // an array of dependencies that esbuild should treat
+      // as external when bundling functions
+      external: [],
+
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app
+      split: false
+    })
+  }
+};
+
