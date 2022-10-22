ae38e7bf76553386c099bd4695dfc821f909ad32
loghomefinishing <74892898+loghomefinishing@users.noreply.github.com>
2022-10-22 16:28:09 -0700

    netadaptor

--- storyblok/svelte.config.js
+++ storyblok/svelte.config.js
@@ -19,21 +19,8 @@ const config = {
 export default {
   kit: {
    
-    // default options are shown
-    adapter: vercel({
-      // if true, will deploy the app using edge functions
-      // (https://vercel.com/docs/concepts/functions/edge-functions)
-      // rather than serverless functions
-      edge: false,
+adapter: adapter()
 
-      // an array of dependencies that esbuild should treat
-      // as external when bundling functions
-      external: [],
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app
-      split: false
-    })
   }
 };
 
