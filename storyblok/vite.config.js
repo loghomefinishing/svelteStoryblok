c7e38f3c13fde09304053b309a3f710ebc24be9c
loghomefinishing <74892898+loghomefinishing@users.noreply.github.com>
2022-10-21 21:49:19 -0600

    vite

--- storyblok/vite.config.js
+++ storyblok/vite.config.js
@@ -1,12 +1,9 @@
+// vite.config.js
 import { sveltekit } from '@sveltejs/kit/vite';
-import mkcert from 'vite-plugin-mkcert'
 
 /** @type {import('vite').UserConfig} */
 const config = {
-    plugins: [sveltekit(), mkcert()],
-    server: {
-        https: true
-    }
+        plugins: [sveltekit()]
 };
 
 export default config;
