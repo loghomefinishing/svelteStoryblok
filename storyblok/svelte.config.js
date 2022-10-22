2871823fc4f42c74e5d5e6551ba63f7611c8c2a4
loghomefinishing <74892898+loghomefinishing@users.noreply.github.com>
2022-10-21 23:33:53 -0600

    netlify

--- storyblok/svelte.config.js
+++ storyblok/svelte.config.js
@@ -19,21 +19,7 @@ const config = {
 export default {
   kit: {
    
-    // default options are shown
-    adapter: vercel({
-      // if true, will deploy the app using edge functions
-      // (https://vercel.com/docs/concepts/functions/edge-functions)
-      // rather than serverless functions
-      edge: false,
-
-      // an array of dependencies that esbuild should treat
-      // as external when bundling functions
-      external: [],
-
-      // if true, will split your app into multiple functions
-      // instead of creating a single one for the entire app
-      split: false
-    })
+    adapter: adapter()
   }
 };
 
