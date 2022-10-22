166669c3e54939acc532b4a6921c1d5edb2d0de9
loghomefinishing <74892898+loghomefinishing@users.noreply.github.com>
2022-10-22 00:00:50 -0600

    hibye

--- storyblok/src/components/Feature.svelte
+++ storyblok/src/components/Feature.svelte
@@ -7,7 +7,7 @@
 <div use:storyblokEditable={blok} class="w-full p-12 bg-[#f7f6fd] rounded-[5px] text-center">
 	<h1 class="text-2xl text-[#1d243d] font-bold">{blok.image}</h1>
 	<h1 class="text-2xl text-[#1d243d] font-bold">{blok.name}</h1>
-<p>hi</p>
+<p>hibye</p>
 </div>
 <div><img src="{blok.pic.filename}" alt="dog"/></div>
 
